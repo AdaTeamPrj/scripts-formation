8aec6e17ff82291f6b310df38e06c6b933a66db0
BoublinFanny <114233180+BoublinFanny@users.noreply.github.com>
2022-10-03 15:05:07 +0200

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+clear #nettoyer l'écran
+
+echo Bonjour a tous!
+echo  #liste vide
+echo J espere que vous allez bien #eviter les accents pour eviter les problemes d'encodage
+
+# sh hello.sh pour lancer la commande
