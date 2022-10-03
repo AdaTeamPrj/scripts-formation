b7ac6b07d02e107d197506f239092dbcc98953aa
AdaTeamPrj <114226218+AdaTeamPrj@users.noreply.github.com>
2022-10-03 15:12:45 +0200

    Delete hello.sh

--- hello.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-clear #nettoyer l'écran
-
-echo Bonjour a tous!
-echo  #liste vide
-echo J espere que vous allez bien #eviter les accents pour eviter les problemes d'encodage
-
-# sh hello.sh pour lancer la commande
